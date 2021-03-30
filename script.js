f1f03d384322e2cc4971fd5486a7d16b5a078f39
Diego Carvalho <diegotunay@gmail.com>
2021-03-30 16:10:53 -0700

    1

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const quizData = [
     {
-        question:'How old is Florin',
+        question:'How old is Florin?',
         a: '10',
-        a: '17',
-        a: '26',
-        a: '110',
+        b: '17',
+        c: '26',
+        d: '110',
         correct: 'c'
     }, {
         question: 'What is the most used programming language in 2019?',
@@ -36,4 +36,53 @@ const quizData = [
         correct: 'd'
    
     }
-]
+];
+
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const d_text = document.getElementById('b_text');
+const b_text = document.getElementById('c_text');
+const c_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected(){
+    const answerEls = document.querySelectorAll('answer');
+    
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked) {
+            return answerEl.id;
+        }
+    });
+
+    return undefined;
+}
+
+submitBtn.addEventListener('click', () => {
+    currentQuiz++;
+
+    const answer = getSelected();
+
+    if(answer){
+        if(currentQuiz < quizData.length) {
+        loadQuiz();
+    }else {
+        // TODO: show results
+        alert('YOU FINISHED!')
+    }
+}
+});
