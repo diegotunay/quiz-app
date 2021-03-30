f0f279032b2d81298eeebc8408fcc1874db64626
Diego Carvalho <diegotunay@gmail.com>
2021-03-30 14:36:37 -0700

    1

---
+++ script.js
@@ -0,0 +1,39 @@
+const quizData = [
+    {
+        question:'How old is Florin',
+        a: '10',
+        a: '17',
+        a: '26',
+        a: '110',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScrip',
+        correct: 'd'
+    }, {
+        question: 'Who is he President of Us?',
+        a: 'Tom Brad',
+        b: 'Joe Biden',
+        c: 'Donald Trump',
+        d: 'Ivan Saldano',
+        correct: 'b'
+    }, {
+        question: 'What does HTML satand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Casding Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals',
+        correct: 'a'
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '1996',
+        b: '1997',
+        c: '1998',
+        d: 'none of the above',
+        correct: 'd'
+   
+    }
+]
